5205652b8c1315c3f37bd9f66a1379c90c7ef03c
Yuugen <yuugenssb@proton.me>
2024-12-01 18:59:02 +0100

    chore: add flake.nix
    Signed-off-by: Yuugen <yuugenssb@proton.me>

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rustacean = {
+      url = "github:mrcjkb/rustaceanvim";
+    };
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , rust-overlay
+    , rustacean
+    , ...
+    } @ inputs:
+    let
+      system = "x86_64-linux";
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs { inherit system overlays; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell rec {
+        buildInputs = with pkgs; [
+          rustacean.packages.${system}.codelldb
+          rust-analyzer
+          rust-bin.stable.latest.default
+        ];
+        shellHook = ''
+          export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
+        '';
+        nativeBuildInputs = with pkgs; [];
+      };
+    };
+}
